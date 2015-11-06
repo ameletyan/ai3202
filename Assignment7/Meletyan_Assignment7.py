8de993d85411b7d4c2a026e120e337c21d119561
Artur Meletyan <artm95@gmail.com>
2015-11-05 23:59:58 -0700

    added conditionals, getters, and setters to Bayes Net class

--- Assignment7/Meletyan_Assignment7.py
+++ Assignment7/Meletyan_Assignment7.py
@@ -7,14 +7,78 @@
 class BayesNet:
 	def __init__(self):
 		self.samples = [0.82, 0.56, 0.08, 0.81, 0.34, 0.22, 0.37, 0.99, 0.55, 0.61, 0.31, 0.66, 0.28, 1.0, 0.95, 0.71, 0.14, 0.1, 1.0, 0.71, 0.1, 0.6, 0.64, 0.73, 0.39, 0.03, 0.99, 1.0, 0.97, 0.54, 0.8, 0.97, 0.07, 0.69, 0.43, 0.29, 0.61, 0.03, 0.13, 0.14, 0.13, 0.4, 0.94, 0.19, 0.6, 0.68, 0.36, 0.67, 0.12, 0.38, 0.42, 0.81, 0.0, 0.2, 0.85, 0.01, 0.55, 0.3, 0.3, 0.11, 0.83, 0.96, 0.41, 0.65, 0.29, 0.4, 0.54, 0.23, 0.74, 0.65, 0.38, 0.41, 0.82, 0.08, 0.39, 0.97, 0.95, 0.01, 0.62, 0.32, 0.56, 0.68, 0.32, 0.27, 0.77, 0.74, 0.79, 0.11, 0.29, 0.69, 0.99, 0.79, 0.21, 0.2, 0.43, 0.81, 0.9, 0.0, 0.91, 0.01]
+		self.C = 0.5		# P(C)
+		self.SC = 0.1		# P(S|C)
+		self.Sc = 0.5		# P(S|~C)
+		self.RC = 0.8		# P(R|C)
+		self.Rc = 0.2		# P(R|~C)
+		self.WSR = 0.99		# P(W|S,R)
+		self.WSr = 0.9		# P(W|S,~R)
+		self.WsR = 0.9		# P(W|~S,R)
+		self.Wsr = 0		# P(W|~S,~R)
 	
 	# GETTERS
 	def getSamples(self):
 		return self.samples
 	
+	def getC(self):
+		return self.C
+	
+	def getSC(self):
+		return self.SC
+	
+	def getSc(self):
+		return self.Sc
+	
+	def getRC(self):
+		return self.RC
+	
+	def getRc(self):
+		return self.Rc
+	
+	def getWSR(self):
+		return self.WSR
+	
+	def getWSr(self):
+		return self.WSr
+	
+	def getWsR(self):
+		return self.WsR
+	
+	def getWsr(self):
+		return self.Wsr
+	
 	# SETTERS
 	def setSamples(self, newSamples):
 		self.samples = newSamples
+	
+	def setC(self, newC):
+		self.C = newC
+	
+	def setSC(self, newSC):
+		self.SC = newSC
+	
+	def setSc(self, newSc):
+		self.Sc = newSc
+	
+	def setRC(self, newRC):
+		self.RC = newRC
+	
+	def setRc(self, newRc):
+		self.Rc = newRc
+	
+	def setWSR(self, newWSR):
+		self.WSR = newWSR
+	
+	def setWSr(self, newWSr):
+		self.WSr = newWSr
+	
+	def setWsR(self, newWsR):
+		self.WsR = newWsR
+	
+	def setWsr(self, newWsr):
+		self.Wsr = newWsr
 
 if __name__ == "__main__":
-	x = 0
+	bn = BayesNet()
+	print(bn.getSamples())
